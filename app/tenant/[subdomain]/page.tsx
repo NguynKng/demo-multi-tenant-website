255e34076fa2c3d2d05218be263c6272d5f47eff
NguynKng <harrybin2109@gmail.com>
2025-11-08 10:53:35 +0700

    change url image preview path

--- app/tenant/[subdomain]/page.tsx
+++ app/tenant/[subdomain]/page.tsx
@@ -54,7 +54,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
       type: "website",
       images: [
         {
-          url: `${Config.NEXT_PUBLIC_BASE_URL}${company.avatar}`,
+          url: company.avatar,
           width: 1200,
           height: 630,
           alt: company.name,
@@ -69,7 +69,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
       description: company.bio,
       images: [
         {
-          url: `${Config.NEXT_PUBLIC_BASE_URL}${company.avatar}`,
+          url: company.avatar,
           width: 1200,
           height: 630,
           alt: company.name,
